694c53d9cf23b1971fd80263c6167c11f26c1a2b
codekiller2000 <50763599+codekiller2000@users.noreply.github.com>
2022-12-10 11:44:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o64ua5nrAzm3dlKiXKyWOijnHWIk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'M5LUuapYXzNMfuLNZTYVrhlVl_vUtHj9mPIipEDvjog',
+      useTemplateId: 't-3dFpn5tyxsdOVeHiLDTkwJgTu-vARZEvZTIir4IHY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'igfNnnNK2ZveHIUIQMNPoxFo9yBOOCgKbtHzgKeZwNA',
 
   CALLBACK_USERS: [
     {
